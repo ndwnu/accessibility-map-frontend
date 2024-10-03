f6325d65d12a50c36dfff51d90a6778a7be30a1e
NDW <167975871+ndwwegkenmerkenenregisters@users.noreply.github.com>
2024-10-03 17:19:46 +0200

    feature(route): #80289 change default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { MapOverviewComponent } from '@modules/map/pages/map/map-overview.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'map', pathMatch: 'full' },
-  { path: 'map', component: MapOverviewComponent },
+  { path: '', component: MapOverviewComponent },
+  { path: '**', redirectTo: '' },
 ];
