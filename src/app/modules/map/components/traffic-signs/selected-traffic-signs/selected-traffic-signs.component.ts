a412a913cd289b1266775685ac25c114ee8ed995
NDW <167975871+ndwwegkenmerkenenregisters@users.noreply.github.com>
2025-06-24 13:20:52 +0200

    feat(link_george): #96709 Fix link to George

--- src/app/modules/map/components/traffic-signs/selected-traffic-signs/selected-traffic-signs.component.ts
+++ src/app/modules/map/components/traffic-signs/selected-traffic-signs/selected-traffic-signs.component.ts
@@ -41,7 +41,9 @@ export class SelectedTrafficSignsComponent implements OnInit {
   );
   georgeTrafficSignUrl = computed(() => `${environment.georgeUrl}/verkeersborden/${this.trafficSign()?.id}`);
   georgeRoadSectionUrl = computed(
-    () => `${environment.georgeUrl}/wegkenmerken/wegvakken/${this.trafficSign()?.roadSectionId}?zoom=true`,
+    () =>
+      `${environment.georgeUrl}/wegkenmerken/wegvakken/${this.trafficSign()
+        ?.roadSectionId}/kenmerken/maximumsnelheid?zoom=true`,
   );
   rvvExemptionUrl = computed(() => {
     const roadOperator = this.roadOperatorService.getRoadOperator(this.trafficSign()?.countyCode ?? '');
